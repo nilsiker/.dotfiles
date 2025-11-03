2f20ac92c0eaa9c17c4b593d6c566e5d812f0d2e
Andreas Nilsson <nilsiker94@gmail.com>
2025-11-03 22:47:11 +0100

    tinker with statusline, remove harpoon

--- nvim/.config/nvim/lua/nilsiker/plugins/mini.lua
+++ nvim/.config/nvim/lua/nilsiker/plugins/mini.lua
@@ -1,8 +1,56 @@
 return {
-    'echasnovski/mini.nvim',
+    'nvim-mini/mini.nvim',
     enabled = true,
     config = function()
+        require 'mini.icons'.setup()
+        require 'mini.git'.setup()
+        require 'mini.diff'.setup()
+
         local statusline = require 'mini.statusline'
-        statusline.setup { use_icons = true }
+
+
+        local active_content = function()
+            local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
+            local git           = statusline.section_git({ trunc_width = 40 })
+            local diff          = statusline.section_diff({ trunc_width = 75 })
+            local diagnostics   = statusline.section_diagnostics({
+                icon = '',
+                trunc_width = 75,
+                signs = {
+                    ERROR = '%#MiniIconsRed#󰅙 ',
+                    INFO = '%#MiniIconsBlue#󰋼 ',
+                    HINT = '%#MiniIconsCyan#󰌵 ',
+                    WARN = '%#MiniIconsYellow# '
+                }
+            })
+            local lsp           = statusline.section_lsp({ trunc_width = 75 })
+            local fileinfo      = statusline.section_fileinfo({ trunc_width = 120 })
+            local location      = statusline.section_location({ trunc_width = 75 })
+            local search        = statusline.section_searchcount({ trunc_width = 75 })
+
+            return statusline.combine_groups({
+                { hl = mode_hl,                 strings = { mode } },
+                { hl = 'MiniStatuslineDevinfo', strings = { diagnostics } },
+                { hl = 'MiniStatuslineDevinfo', strings = { lsp } },
+                '%<', -- Mark general truncate point
+                { hl = 'MiniStatuslineFilename', strings = { '%t' } },
+                '%=', -- End left alignment
+                { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+                { hl = mode_hl,                  strings = { git, diff } },
+            })
+        end
+
+        local s = ''
+
+        local active_content_string = active_content()
+        print(active_content_string)
+
+        statusline.setup {
+            use_icons = true,
+            content = {
+                active = active_content,
+                inactive = active_content
+            }
+        }
     end
 }
--- nvim/.config/nvim/lua/nilsiker/plugins/vim-fugitive.lua
+++ nvim/.config/nvim/lua/nilsiker/plugins/vim-fugitive.lua
@@ -1,13 +1,4 @@
 return {
     "tpope/vim-fugitive",
-    config = function()
-        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-        vim.keymap.set("n", "<leader>gA", function() vim.cmd("Git add *") end)
-        vim.keymap.set("n", "<leader>gd", function() vim.cmd("Git diff") end)
-        vim.keymap.set("n", "<leader>gC", function() vim.cmd("Git commit") end)
-        vim.keymap.set("n", "<leader>gP", function() vim.cmd("Git push") end)
-        vim.keymap.set("n", "<leader>gb", function() vim.cmd("Git branch") end)
-        vim.keymap.set("n", "<leader>gc", function() vim.cmd("Git checkout " .. vim.fn.input("Checkout: ")) end)
-    end
-
+    config = function() end
 }
